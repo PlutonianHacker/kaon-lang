c81ff5aba2bf2310d5af03b2e4ee318e752c619b
PlutonianHacker <edmondjames16@gmail.com>
2021-12-27 20:48:26 -0500

    Initial Commit

---
+++ src/parser.rs
@@ -0,0 +1,12 @@
+use crate::token::Token;
+
+struct Parser {
+    tokens: Vec<Token>,
+    pos: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser { tokens, pos: 0 }
+    }
+}
