24d587f95546cd9e1418867e3d4cbaacfabfa66c
PlutonianHacker <edmondjames16@gmail.com>
2022-02-23 13:05:24 -0500

    Inital commit

---
+++ src/common/loader.rs
@@ -0,0 +1,22 @@
+use super::module::Module;
+use crate::error::Error;
+
+pub struct Loader {
+    pub cache: Vec<Module>,
+}
+
+impl Loader {
+    pub fn new() -> Self {
+        Self {
+            cache: Vec::new(),
+        }
+    }
+
+    pub fn compile_module(&mut self, _name: &str) -> Result<Module, Error> {
+        todo!()
+    }
+
+    pub fn get_module(&self, name: &str) -> Option<&Module> {
+        self.cache.iter().find(|m| m.name == name)
+    }
+}
